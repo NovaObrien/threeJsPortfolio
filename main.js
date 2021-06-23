310814ae329abaef684711d2c560a650bede7cf7
Nova (Tanner) O'Brien <tannerkobrien@gmail.com>
2021-06-22 20:08:45 -0600

    First attempt at threejs

---
+++ main.js
@@ -0,0 +1,89 @@
+import './style.css'
+
+import * as THREE from 'three'
+
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+})
+
+renderer.setPixelRatio( window.devicePixelRatio)
+renderer.setSize(window.innerWidth, window.innerHeight)
+camera.position.setZ(30)
+
+renderer.render(scene, camera)
+
+// add avatar //
+const novaTexture = new THREE.TextureLoader().load('img/seflPort2.png')
+const nova = new THREE.Mesh(
+
+   new THREE.BoxGeometry(5,5,5),
+   new THREE.MeshBasicMaterial({ map: novaTexture})
+  )
+
+
+scene.add(nova)
+
+// add light //
+function addLight() {
+
+  const pointLight = new THREE.PointLight(0x6DF0DA)
+  pointLight.position.set(20,20,20)
+  
+  const ambientLight = new THREE.AmbientLight(0xffffff)
+  
+  scene.add(pointLight, ambientLight)
+  
+  const lightHelper = new THREE.PointLightHelper(pointLight)
+  const gridHelper = new THREE.GridHelper()
+  
+  scene.add(lightHelper, gridHelper)
+}
+
+const controls = new OrbitControls(camera, renderer.domElement)
+
+function addStar() {
+  const geometry = new THREE.BoxGeometry(3, 3, 3)
+  const material = new THREE.MeshStandardMaterial( { color: 0x6DF0DA})
+  const star = new THREE.Mesh(geometry, material)
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100))
+
+  star.position.set(x,y,z)
+  scene.add(star)
+}
+
+
+
+
+const spaceTexture  = new THREE.TextureLoader().load('img/space.jpg')
+scene.background = spaceTexture
+
+const starsArr = new Array(200).fill().forEach(addStar)
+function init() {
+  addLight()
+}
+
+function animate() {
+  requestAnimationFrame(animate)
+
+  // for(let i = 0; i < starsArr.length; i++){
+  //   starsArr[i].x += 10
+  // }
+  
+  nova.rotation.x += 0.01
+  nova.rotation.y += 0.005
+  nova.rotation.z += 0.01
+
+  controls.update()
+
+  renderer.render( scene, camera)
+}
+
+init()
+animate()
